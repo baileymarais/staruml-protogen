70be11425a0e0ad70065204b5834bc9617675e3c
Masun <maxun@live.cn>
2017-12-05 13:23:40 +0800

    使其支持optional和required关键字
    在StarUML中，勾选nullable复选框，使协议中某一个字段变为optional关键词，否则胃required

--- generator.js
+++ generator.js
@@ -125,7 +125,7 @@ define(function (require, exports, module) {
 
       if (index) generator.writeLine();
       if (field.documentation) { _writeComment(generator, field.documentation); }
-      generator.writeLine(dataType + ' ' + field.name + ' = ' + tagNum + ';');
+      generator.writeLine((field.nullable?'optional ':'required ') + dataType + ' ' + field.name + ' = ' + tagNum + ';');
     });
     generator.outdent();
     generator.writeLine('}');
